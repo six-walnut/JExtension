cfeda895bd1e11cf490fbd084c5f44b0314da9e8
sixwalnut <782652987@qq.com>
2020-07-27 12:52:49 +0800

    first commit

---
+++ Example/Pods/Target Support Files/JExtension/JExtension-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JExtension : NSObject
+@end
+@implementation PodsDummy_JExtension
+@end
---
+++ JExtension/Classes/UIFont+LJExtension.swift
@@ -0,0 +1,36 @@
+//
+//  UIFont+Extension.swift
+//  FindNumber
+//
+//  Created by 靖核 on 2020/6/16.
+//  Copyright © 2020 靖核. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    ///细体
+    class func pingFangSC_Light (size:CGFloat) -> UIFont {
+        return  UIFont(descriptor: UIFontDescriptor(fontAttributes: [ UIFontDescriptor.AttributeName.name : "PingFangSC-Light" ]), size: size)
+    }
+    ///中黑
+    class func pingFangSC_Medium (size:CGFloat) -> UIFont {
+        return  UIFont(descriptor: UIFontDescriptor(fontAttributes: [ UIFontDescriptor.AttributeName.name : "PingFangSC-Medium" ]), size: size)
+    }
+    ///常规
+    class func pingFangSC_Regular (size:CGFloat) -> UIFont {
+        return  UIFont(descriptor: UIFontDescriptor(fontAttributes: [ UIFontDescriptor.AttributeName.name : "PingFangSC-Regular" ]), size: size)
+    }
+    ///中粗
+    class func pingFangSC_Semibold (size:CGFloat) -> UIFont {
+        return  UIFont(descriptor: UIFontDescriptor(fontAttributes: [ UIFontDescriptor.AttributeName.name : "PingFangSC-Semibold" ]), size: size)
+    }
+    ///纤细
+    class func pingFangSC_Thin (size:CGFloat) -> UIFont {
+        return  UIFont(descriptor: UIFontDescriptor(fontAttributes: [ UIFontDescriptor.AttributeName.name : "PingFangSC-Thin" ]), size: size)
+    }
+    ///极细
+    class func pingFangSC_Ultralight (size:CGFloat) -> UIFont {
+        return  UIFont(descriptor: UIFontDescriptor(fontAttributes: [ UIFontDescriptor.AttributeName.name : "PingFangSC-Ultralight" ]), size: size)
+    }
+}
